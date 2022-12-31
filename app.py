4c524a34d521856012fd667dcf4fe28de904bdab
ameerparial <111529603+ameerparial@users.noreply.github.com>
2022-12-31 09:53:43 +0500

    app.py modified

--- app.py
+++ app.py
@@ -1,4 +1,36 @@
 import streamlit as st
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+#title
+st.title("Eye Disease Classification")
+
+#subtitle
+st.markdown("## Machine Learning Project")
+
+st.markdown("Link to the app - [eye-disease-classification on 🤗 Spaces](https://huggingface.co/spaces/ameerhamza/eye-disease-classification)")
+
+#image uploader
+image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+
+
+# @st.cache
+# def load_model(): 
+#     reader = ocr.Reader(['en'],model_storage_directory='.')
+#     return reader 
+
+# reader = load_model()
+
+
+if image is not None:
+
+    input_image = Image.open(image) #read image
+    st.image(input_image) #display image
+
+    with st.spinner("🤖 AI is at Work! "):        
+        label = 'Ameer Hamza'
+        st.write(label)
+    #st.success("Here you go!")
+    st.balloons()
+else:
+    st.write("Upload an Image")
+
+st.caption("Made by Ameer Hamza, Thanks to Hugging faces")
